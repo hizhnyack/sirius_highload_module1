73af75c9d2837458fb3a95b1ea4d109e0489fc24
user107 <khizhnyak.yp@gmail.com>
2025-03-14 21:51:46 +0300

    2

---
+++ src/main/java/ru/hpclab/hl/module1/repository/SaleRepository.java
@@ -0,0 +1,25 @@
+package ru.hpclab.hl.module1.repository;
+
+import ru.hpclab.hl.module1.model.Sale;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class SaleRepository {
+    private static final List<Sale> sales = new ArrayList<>();
+
+    public List<Sale> findAll() {
+        return sales;
+    }
+
+    public void save(Sale sale) {
+        sales.add(sale);
+    }
+
+    public Optional<Sale> findById(Long id) {
+        return sales.stream()
+                .filter(sale -> sale.getId().equals(id))
+                .findFirst();
+    }
+}
