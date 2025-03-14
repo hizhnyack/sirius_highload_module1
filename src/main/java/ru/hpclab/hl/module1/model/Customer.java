870881552535c022f32cd7867a1f18c01f276450
user107 <khizhnyak.yp@gmail.com>
2025-03-14 21:42:47 +0300

    1

---
+++ src/main/java/ru/hpclab/hl/module1/model/Customer.java
@@ -0,0 +1,80 @@
+package ru.hpclab.hl.module1.model;
+
+import java.util.Objects;
+
+public class Customer {
+    private Long id;
+    private String fullName;
+    private String phone;
+    private boolean hasDiscountCard;
+
+    // Конструкторы
+    public Customer() {}
+
+    public Customer(Long id, String fullName, String phone, boolean hasDiscountCard) {
+        this.id = id;
+        this.fullName = fullName;
+        this.phone = phone;
+        this.hasDiscountCard = hasDiscountCard;
+    }
+
+    // Геттеры и сеттеры
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isHasDiscountCard() {
+        return hasDiscountCard;
+    }
+
+    public void setHasDiscountCard(boolean hasDiscountCard) {
+        this.hasDiscountCard = hasDiscountCard;
+    }
+
+    // equals, hashCode, toString
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return hasDiscountCard == customer.hasDiscountCard &&
+                Objects.equals(id, customer.id) &&
+                Objects.equals(fullName, customer.fullName) &&
+                Objects.equals(phone, customer.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, fullName, phone, hasDiscountCard);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", phone='" + phone + '\'' +
+                ", hasDiscountCard=" + hasDiscountCard +
+                '}';
+    }
+}
