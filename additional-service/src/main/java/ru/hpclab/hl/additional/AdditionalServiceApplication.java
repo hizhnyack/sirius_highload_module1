0e6970cb535a6975ab2d2667872e65cbd4f44e78
hizhnyack <khizhnyak.yp@gmail.com>
2025-05-05 00:03:46 +0300

    logs

--- additional-service/src/main/java/ru/hpclab/hl/additional/AdditionalServiceApplication.java
+++ additional-service/src/main/java/ru/hpclab/hl/additional/AdditionalServiceApplication.java
@@ -3,9 +3,11 @@ package ru.hpclab.hl.additional;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 public class AdditionalServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdditionalServiceApplication.class, args);
