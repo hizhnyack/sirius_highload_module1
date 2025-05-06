c6eb20c1e44b9262934202a1b21a514e31435f39
hizhnyack <khizhnyak.yp@gmail.com>
2025-05-06 15:36:22 +0300

    fixed cache

--- additional-service/src/main/java/ru/hpclab/hl/additional/service/StatisticsCacheService.java
+++ additional-service/src/main/java/ru/hpclab/hl/additional/service/StatisticsCacheService.java
@@ -6,6 +6,9 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import ru.hpclab.hl.additional.dto.AverageWeightResponse;
+import ru.hpclab.hl.additional.dto.ProductDTO;
+import ru.hpclab.hl.additional.dto.CustomerDTO;
+import ru.hpclab.hl.additional.dto.SaleDTO;
 
 import java.util.List;
 import java.util.Map;
@@ -15,6 +18,9 @@ import java.util.concurrent.ConcurrentHashMap;
 @Component
 public class StatisticsCacheService {
     private final Map<String, List<AverageWeightResponse>> averageWeightCache = new ConcurrentHashMap<>();
+    private final Map<String, ProductDTO> productCache = new ConcurrentHashMap<>();
+    private final Map<String, CustomerDTO> customerCache = new ConcurrentHashMap<>();
+    private final Map<String, SaleDTO> salesCache = new ConcurrentHashMap<>();
 
     @Value("${statistics.cache.info:Cache Statistics}")
     private String infoString;
@@ -29,6 +35,42 @@ public class StatisticsCacheService {
 
     public boolean hasAverageWeight(String key) {
         return averageWeightCache.containsKey(key);
+    }
+
+    public void putProduct(String id, ProductDTO product) {
+        productCache.put(id, product);
+    }
+
+    public ProductDTO getProduct(String id) {
+        return productCache.get(id);
+    }
+
+    public boolean hasProduct(String id) {
+        return productCache.containsKey(id);
+    }
+
+    public void putCustomer(String id, CustomerDTO customer) {
+        customerCache.put(id, customer);
+    }
+
+    public CustomerDTO getCustomer(String id) {
+        return customerCache.get(id);
+    }
+
+    public boolean hasCustomer(String id) {
+        return customerCache.containsKey(id);
+    }
+
+    public void putSale(String id, SaleDTO sale) {
+        salesCache.put(id, sale);
+    }
+
+    public SaleDTO getSale(String id) {
+        return salesCache.get(id);
+    }
+
+    public boolean hasSale(String id) {
+        return salesCache.containsKey(id);
     }
 
     @Async
